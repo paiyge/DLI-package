991e1fae8461075b595db8d39ff57805ae820c59
paiyge <91477408+paiyge@users.noreply.github.com>
2022-07-25 10:09:23 -0400

    Add files via upload
    Beginning of calculations

---
+++ calculate.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+import scrape
+import pipe
+
+def create_df():
+    '''
+
+    :return: par_df
+    '''
+    df = scrape.df
+
+    par_df = df[['pst_time', 'sr_1']].copy()
+    par_df = par_df.dropna()
+    par_df = par_df.rename(columns={'pst_time': 'DateTime', 'sr_1': 'PAR'})
+    return par_df
+
+
+def calculate_ppfd(par_df):
+    '''
+
+    :return:ppfd_df
+    '''
+    df = par_df.copy()
+    df["Date"] = pd.to_datetime(df['DateTime']).dt.date
+    #ppfd = df.groupby(['Date'])['PAR'].mean()
+    #ppfd_df = pd.DataFrame("")
+    ppfd_df = pd.DataFrame(df.groupby(['Date'])['PAR'].mean())
+    #pd.DataFrame({"Date": df.groupby(by="Date")})
+
+    print(ppfd_df)
+    #return ppfd
+
+
+if __name__ == "__main__":
+    calculate_ppfd(create_df())
