91d8de4454035603b3cd4782af6770faff2c78c5
Mewsp <108479142+Mewsp@users.noreply.github.com>
2022-08-02 15:14:53 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,24 @@
-from re import X
-
-
 exec(open('./Scrapy_updatedNames.py').read())
 
 if __name__ == "__main__":
-    for f in X:
-        """
-        for fn in (do, do2, do3, do4):
-    try:
-        fn()
-        break
-    except:
-        continue
-        """
+    cammands=[(4,5,2010,10,17,2010),("June",5,2010,10,17,2010),(4,5,10,10,17,10),
+              (4,50,2010,10,17,2010),(5,2010,10,17,2010)]
+    #examples of [missing data inside range, string instead of int, abreviated year,
+    #miss type from day, skipped from month]
+    #[True, False, True, False, False]
+    
+    #datetime handels a lot of the errors!
+    
     s=Sun_expo()
-    #data_range(from month, from day, from year, to_month, to_day, to_year)
-    print("example of intended use:")
-    s.data_range(4,5,2010,10,17,2010) #Done!
+    rate=[]
+    for f in cammands:
+        try:
+            s.data_range(*f)
+            rate.append("Pass")
+            continue
+        except Exception as e:
+            print("Error:", e)
+            rate.append("Fail")
+            continue
 
-    #if string entered for month:
-    print("string error:")
-    s.data_range("June",5,2010,10,17,2010) #an integer is required
-
-    #if year is abbreviated:
-    print("shortened year error: ")
-    s.data_range(4,5,2010,10,17,2010) #an integer is required
+    print(rate)
