efea171b0ecf50e319558b2594c8f92f680ff076
Mewsp <108479142+Mewsp@users.noreply.github.com>
2022-08-05 11:40:12 -0700

    Update Scrapy_updatedNames.py

--- Scrapy_updatedNames.py
+++ Scrapy_updatedNames.py
@@ -44,7 +44,3 @@ class Sun_expo:
             print(df['pst_time'].iloc[0])
             print(df['pst_time'].iloc[-1])
             return print(df)
-
-
-s=Sun_expo()
-s.data_range(4,5,2010,5,17,2010)
