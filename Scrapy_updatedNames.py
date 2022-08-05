31b987e55c0fc94695e01e0624e9a15d99685713
Mewsp <108479142+Mewsp@users.noreply.github.com>
2022-08-05 14:44:45 -0700

    Fixed

--- Scrapy_updatedNames.py
+++ Scrapy_updatedNames.py
@@ -15,32 +15,28 @@ class Sun_expo:
             print("data is only available from ", oldest_data," to present.")
         else:
             dfs=[]
-            print("compiling data between",from_date,"-",to_date,"please wait a moment.")
+            print("Compiling data between",from_date,"-",to_date,"please wait a moment.")
             #Iterates through links and converts to pandas data frame.
-            while from_date<=to_date:
+            first_date=from_date-relativedelta(months=1)
+            while first_date<=to_date:
                 #iterate count up by month
-                from_date += relativedelta(months=1)
-                year=from_date.strftime("%Y")
-                month=datetime.date(day=1,month=from_date.month-1,year=from_date.year).strftime("%m")
+                first_date += relativedelta(months=1)
+                year=first_date.strftime("%Y")
+                month=first_date.strftime("%m")
                 try:
                     dfs.append(pd.read_csv('http://pubdata.mlml.calstate.edu/mlml_last/weather/'+year+"-"+month+".csv"))
                 except: #if data is missing pass
                     pass
-
-            #converts from_date and to_time back to original.
-            from_date=datetime.datetime(from_y,from_m,from_d)
-            to_date=datetime.datetime(to_y,to_m,to_d)
 
             #compiles all pulled csv files
             df = pd.concat(dfs, ignore_index=True)
             #converts column to datetime
             df['pst_time']=pd.to_datetime(df['pst_time'])
             #filters data frame by day
-            df=df[(df['pst_time']>=from_date)&(df['pst_time']<=to_date+relativedelta(days=1))]
+            df=df[(df['pst_time']>=pd.to_datetime(from_date))&(df['pst_time']<=pd.to_datetime(to_date+relativedelta(days=1)))]
 
             print("Done!")
-
-            pd.DataFrame(df).sort_values(by="pst_time")
-            print(df['pst_time'].iloc[0])
-            print(df['pst_time'].iloc[-1])
             return df
+
+s=Sun_expo()
+s.data_range(11,5,2010,2,4,2011)
